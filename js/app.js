75ec463d10bebdb5e0ec1a09c47aa58fd95149a0
Dina <dina.yamanid@gmail.com>
2023-05-03 11:54:05 +0200

    finished with clean code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 // Tu código aquí dina.
+
+// Variable de estado: un número para almacenar el valor actual del contador
+
+let counter = 0;
+const number = document.querySelector("#counter");
+
+
+//Seleccionar el botón ADDCOUNT
+
+const changeColor = () => {
+  if (counter > 0) {
+    number.style.color = "green";
+  } else if (counter < 0) {
+    number.style.color = "red";
+  } else {
+    number.style.color = "#333333";
+  }
+};
+
+document.querySelector(".nextBtn").addEventListener("click", () => {
+  counter++;
+  number.textContent = counter;
+  changeColor();
+});
+
+document.querySelector(".prevBtn").addEventListener("click", () => {
+  --counter;
+ number.textContent = counter;
+  changeColor();
+});
